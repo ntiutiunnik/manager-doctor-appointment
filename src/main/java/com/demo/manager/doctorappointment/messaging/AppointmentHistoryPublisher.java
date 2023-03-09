3cb7920ad4e81edb78eca31010af10b174db7977
ntiutiunnik <puffbeatsale@gmail.com>
2023-03-09 18:11:22 +0400

    Kafka deployment config added.

--- src/main/java/com/demo/manager/doctorappointment/messaging/AppointmentHistoryPublisher.java
+++ src/main/java/com/demo/manager/doctorappointment/messaging/AppointmentHistoryPublisher.java
@@ -2,6 +2,7 @@ package com.demo.manager.doctorappointment.messaging;
 
 import com.demo.manager.doctorappointment.dto.impl.AppointmentDto;
 import com.demo.manager.doctorappointment.messaging.impl.KafkaPublisher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import java.util.List;
 @Component
 public class AppointmentHistoryPublisher implements KafkaPublisher<AppointmentDto> {
 
-//    @Value("${spring.kafka.topic.name}")
+    @Value("${spring.kafka.topic.name}")
     private String topicName;
 
     private final KafkaTemplate<String, AppointmentDto> kafkaTemplate;
